321384ed41b8ef2466878d4795e5cf6c0f242e73
Edder <enaranjo5489@uta.edu.ec>
2025-06-05 10:50:02 -0500

    Actualizacion de Rutas de Buses

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { authRoutes } from './core/login/auth.routes';
 import { homePageRoutes } from '@features/homepage/hompage.routes';
 import { coopsRoutes } from '@features/coops/coops.routes';
+import { busesRoutes } from '@features/buses/buses.routes';
 
 export const routes: Routes = [
   ...authRoutes,
   ...homePageRoutes,
   ...coopsRoutes,
+  ...busesRoutes,
   {
     path: '',
     redirectTo: '/login',
